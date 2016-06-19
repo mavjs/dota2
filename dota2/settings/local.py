067d4cf918905c02367b48f711ee133f70b4c738
Maverick <mavjs01@gmail.com>
2016-06-19 17:39:20 +0200

    use postgresql on local dev

--- dota2/settings/local.py
+++ dota2/settings/local.py
@@ -6,7 +6,11 @@ SECRET_KEY = '3x@4#rx0fwt15gf-&@=0f3xzxrto_oz2r3=&zdc5mg(&)vhi-t'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dota2db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }

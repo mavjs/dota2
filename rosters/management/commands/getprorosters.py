a15cc980ab3c66faba7ab1da5a25ba1807454b7a
Maverick <mavjs01@gmail.com>
2016-07-03 19:20:45 +0200

    make changes to the model
    * allow for updating MMR and Team tags

--- rosters/management/commands/getprorosters.py
+++ rosters/management/commands/getprorosters.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
-from rosters.tasks import scrape
+from rosters.tasks import get_rosters
 
 
 class Command(BaseCommand):
     help = 'Scrape Dota2 Pro Registration list'
 
     def handle(self, *args, **options):
-        scrape()
+        get_rosters()
--- rosters/models.py
+++ rosters/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Team(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=1024)
+    tag = models.CharField(max_length=20, blank=True, null=True)
 
 
 class Player(models.Model):
@@ -22,4 +23,7 @@ class Player(models.Model):
         default='Primary',
         max_length=1024,
     )
+    country = models.CharField(max_length=4, null=True, blank=True)
+    rank = models.IntegerField(null=True, blank=True)
+    mmr = models.IntegerField(null=True, blank=True)
     updated = models.DateTimeField(blank=True, null=True)

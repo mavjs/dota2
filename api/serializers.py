bb44cb0c277cbf6b34ace7b393426fadd681a04f
Maverick <mavjs01@gmail.com>
2016-07-03 19:22:45 +0200

    add depth to serializers

--- api/serializers.py
+++ api/serializers.py
@@ -5,8 +5,10 @@ from rosters.models import Player, Team
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Team
+        depth = 1
 
 
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Player
+        depth = 1
